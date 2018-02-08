32a041a873ca07e70c77341e50661f83eb197883
tB1ack <31832634+tB1ack@users.noreply.github.com>
2018-02-08 15:57:47 -0800

    Add files via upload

---
+++ challengeDaysBetweenDates/default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="challengeDaysBetweenDates._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How many days have elapsed?<br />
+            <br />
+            Choose one date:<br />
+            <asp:Calendar ID="firstCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Choose a second date:<br />
+            <asp:Calendar ID="secondCalendar" runat="server"></asp:Calendar>
+            <br />
+            <asp:Button ID="runButton" runat="server" OnClick="runButton_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ challengeDaysBetweenDates/default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace challengeDaysBetweenDates
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void runButton_Click(object sender, EventArgs e)
+        {
+            if (firstCalendar.SelectedDate > secondCalendar.SelectedDate)
+            {
+                resultLabel.Text = firstCalendar.SelectedDate
+                    .Subtract(secondCalendar.SelectedDate)
+                    .TotalDays.ToString();
+            } else
+            {
+                resultLabel.Text = secondCalendar.SelectedDate
+                    .Subtract(firstCalendar.SelectedDate)
+                    .TotalDays.ToString();
+            }
+
+        }
+    }
+}
